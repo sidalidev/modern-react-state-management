e88a2177594f49defcddd421454b7c1a0b45052a
Sid Bentifraouine <sidbentifraouine@gmail.com>
2020-06-30 17:26:31 +0200

    11-context-api-non-optim

--- src/components/CountDisplay.tsx
+++ src/components/CountDisplay.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { memo } from "react";
 import { useCountState } from "../contexts/count-context";
 
-const CountDisplay = () => {
+const Double = memo(() => {
   const { count } = useCountState();
+  const doubledCount = count;
 
-  return <h3>{count}</h3>;
-};
+  return <span>{doubledCount}</span>;
+});
+
+const CountDisplay = memo(() => {
+  return (
+    <h3>
+      Double = <Double />
+    </h3>
+  );
+});
 
 export default CountDisplay;
